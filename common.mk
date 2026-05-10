d9cadacf2359a916dd38e6d82f973fba0e762ac5
Flopster101 <nahuelgomez329@gmail.com>
2026-05-10 20:09:02 -0300

    s5e8825: Remove useless fastbootd mock HAL
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- common.mk
+++ common.mk
@@ -28,9 +28,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+PRODUCT_PACKAGES += fastbootd
 
 # Some useful binaries
 PRODUCT_PACKAGES += shrink_common bxhsed_common
