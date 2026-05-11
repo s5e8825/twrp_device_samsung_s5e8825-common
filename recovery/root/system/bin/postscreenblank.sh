9de6fe25eabcc44c4a67b5036d0d94e6a2b713a0
Flopster101 <nahuelgomez329@gmail.com>
2026-05-11 01:06:07 -0300

    s5e8825: root: Fix DT2W paths and values for s5e8825
    This platform actually uses slightly different paths and different values.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- recovery/root/system/bin/postscreenblank.sh
+++ recovery/root/system/bin/postscreenblank.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
 
-TSP_INPUT="/sys/class/sec/tsp/input/enabled"
+TSP_INPUT="/sys/class/sec/tsp/enabled"
 TSP_CMD="/sys/class/sec/tsp/cmd"
 
 if [ -w "$TSP_INPUT" ]; then
-	echo 0 > "$TSP_INPUT"
+	echo 1,0 > "$TSP_INPUT"
 fi
 
 if [ -w "$TSP_CMD" ]; then
--- recovery/root/system/bin/postscreenunblank.sh
+++ recovery/root/system/bin/postscreenunblank.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 
-TSP_INPUT="/sys/class/sec/tsp/input/enabled"
+TSP_INPUT="/sys/class/sec/tsp/enabled"
 
 if [ -w "$TSP_INPUT" ]; then
-	echo 1 > "$TSP_INPUT"
+	echo 2,1 > "$TSP_INPUT"
 fi
