26b7ba3ffe9c70e47bbf968d85c4f451fa2177e4
Flopster101 <nahuelgomez329@gmail.com>
2026-05-16 04:05:05 -0300

    s5e8825: postscreen*blank: Add delay before writing to TSP sysfs nodes
    This should help with it sometimes failing to enter low power mode.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- recovery/root/system/bin/postscreenblank.sh
+++ recovery/root/system/bin/postscreenblank.sh
@@ -4,9 +4,11 @@ TSP_INPUT="/sys/class/sec/tsp/enabled"
 TSP_CMD="/sys/class/sec/tsp/cmd"
 
 if [ -w "$TSP_INPUT" ]; then
+	sleep 0.1
 	echo 1,0 > "$TSP_INPUT"
 fi
 
 if [ -w "$TSP_CMD" ]; then
+	sleep 0.1
 	echo aot_enable,1 > "$TSP_CMD"
 fi
--- recovery/root/system/bin/postscreenunblank.sh
+++ recovery/root/system/bin/postscreenunblank.sh
@@ -3,5 +3,6 @@
 TSP_INPUT="/sys/class/sec/tsp/enabled"
 
 if [ -w "$TSP_INPUT" ]; then
+	sleep 0.1
 	echo 2,1 > "$TSP_INPUT"
 fi
